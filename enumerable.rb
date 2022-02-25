f333ae90e92340a7b487bada8a305e066894c665
mongare <mongaresams@gmail.com>
2022-02-25 15:22:41 +0300

    Add code for my enumerable

---
+++ enumerable.rb
@@ -0,0 +1,19 @@
+module Enumerable
+    def all?(&block)
+      res = true
+      each { |item| res = false unless block.call(item) }
+      res
+    end
+  
+    def any?(&block)
+      res = false
+      each { |item| res = true if block.call(item) }
+      res
+    end
+  
+    def filter(&block)
+      res = []
+      each { |item| res << item if block.call(item) }
+      res
+    end
+  end
